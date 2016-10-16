b12d772705ed379385e4a9a9ea449ea7eee9c6c8
akeem-s <akeemstreet@gmail.com>
2016-10-15 17:06:16 -0700

    Form actions for input added as well as reducer cases

--- src/components/userLogin/UserLogin.js
+++ src/components/userLogin/UserLogin.js
@@ -5,17 +5,34 @@ import * as actions from '../../actions/actions.js'
 export class UserLogin extends React.Component{
    constructor(props){
      super(props)
-     const { dispatch, userLoginReducer } = this.props
-     dispatch(actions.userLoginActions.createUser()).then((data) => {
-       console.log(data)
-     })
    }
 
    handleSubmit(e){
     let name = e.target.name.value
     let email = e.target.email.value
     let phone = e.target.phone.value
+
+    const { dispatch, userLoginReducer } = this.props
+
+    dispatch(actions.userLoginActions.createUser(name, email, phone)).then((data) => {
+      console.log(data)
+    })
    }
+
+    userNameChange(e){
+  		const {dispatch, userLoginReducer} = this.props
+  		dispatch(actions.userLoginActions.userNameChange(e.target.value))
+  	}
+
+    userEmailChange(e){
+      const {dispatch, userLoginReducer} = this.props
+  		dispatch(actions.userLoginActions.userEmailChange(e.target.value))
+    }
+
+    userPhoneChange(e){
+      const {dispatch, userLoginReducer} = this.props
+  		dispatch(actions.userLoginActions.userPhoneChange(e.target.value))
+    }
 
    render(){
      return(
@@ -23,13 +40,13 @@ export class UserLogin extends React.Component{
          <h1> Create User </h1>
 
           <form className="userLoginForm" onSubmit={this.handleSubmit.bind(this)}>
-            <input className="name" type="text" name="name" placeholder="Name" />
+            <input className="name" type="text" name="name" placeholder="Name" onChange={this.userNameChange.bind(this)}/>
             <br/>
-            <input className="email" type="text" name="email" placeholder="Email" />
+            <input className="email" type="text" name="email" placeholder="Email" onChange={this.userEmailChange.bind(this)}/>
             <br/>
-            <input className="phone" type="password" name="phone" placeholder="Phone" />
+            <input className="phone" type="password" name="phone" placeholder="Phone" onChange={this.userPhoneChange.bind(this)}/>
             <br/>
-            <input type="submit" />
+            <input type="submit" onSubmit={this.handleSubmit.bind(this)}/>
           </form>
        </div>
      )
